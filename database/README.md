963e536988c3997e72ba672d43041f9a065b6969
shadowman11 <vashmav@outlook.com>
2025-12-03 12:06:37 -0800

    Configured Supabase with basic functions

---
+++ database/README.md
@@ -0,0 +1,7 @@
+# Database
+
+### Run code
+Run `npm run dev` to run the test file that adds two rows to the Topics database, then queries them
+
+### TODO
+* Configure Row Level Security (RLS) policies on the Topics table for security.
---
+++ database/src/db.ts
@@ -0,0 +1,50 @@
+import { createClient } from '@supabase/supabase-js';
+import "dotenv/config";
+
+const sbUrl = 'https://vgagxddospjhsebqpbqa.supabase.co';
+const sbKey = process.env.SUPABASE_KEY;
+if (sbKey === undefined) {
+    throw new Error("Could not load Supabase API key from environment variables");
+}
+const sb = createClient(sbUrl, sbKey);
+
+// Add a new topic to the database with its title, authors, summary, source and category.
+async function dbInsertTopic(title: String, authors: String, summary: String, source_link: String, category: String) {
+    try {
+        let res = await sb.from("Topics").insert(
+            {
+                title: title.toLowerCase(), 
+                authors: authors.toLowerCase(), 
+                summary: summary, 
+                source_link: source_link, 
+                category: category.toLowerCase()
+            }
+        )
+        console.log(res)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+// Get all topics from the database
+async function dbGetAll() {
+    try { 
+        let res = sb.from("Topics").select();
+        console.log(res)
+        return res;
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+// Get all topics with a matching category from the database.
+async function dbGetCategory(category: String) {
+    return sb.from("Topics").select().eq("category", category.toLowerCase())
+}
+
+// Get all topics with a matching author from the database.
+async function dbGetAuthors(authors: String) {
+    return sb.from("Topics").select().eq("authors", authors.toLowerCase())
+}
+
+export {dbInsertTopic, dbGetAll, dbGetAuthors, dbGetCategory}
