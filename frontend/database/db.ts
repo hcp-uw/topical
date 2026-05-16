4b3adebe5b11d02372f1a66562ffe2989faa5e64
shadowman11 <vashmav@outlook.com>
2026-05-15 19:53:10 -0700

    Added refresh all pages and pagination for search page

--- frontend/database/db.ts
+++ frontend/database/db.ts
@@ -62,9 +62,12 @@ async function dbGetN(uid: String, offset: number, n: number, categories: string
     }
 }
 
-async function dbSearch(searchTerm: String) {
+async function dbSearchN(searchTerm: String, offset: number, n: number) {
     try {
-        const res = await sb.from("Topics").select().or(`title.ilike.*${searchTerm}*,original_title.ilike.*${searchTerm}*,authors.ilike.*${searchTerm}*,summary.ilike.*${searchTerm}*,category.ilike.*${searchTerm}*`)
+        const res = await sb.from("Topics").select()
+            .or(`title.ilike.*${searchTerm}*,original_title.ilike.*${searchTerm}*,authors.ilike.*${searchTerm}*,summary.ilike.*${searchTerm}*,category.ilike.*${searchTerm}*`)
+            .order("created_at", {ascending: false, nullsFirst: false})
+            .range(offset, offset + n - 1);
 
         return res;
     } catch (e) {
@@ -136,4 +139,4 @@ async function dbGetUserLikes(userId: string) {
 }
 
 
-export {sb, dbInsertTopic, dbGetTopic, dbGetN, dbSearch, dbGetCategories, dbGetLiked, dbSetLiked, dbGetUserLikes}
+export {sb, dbInsertTopic, dbGetTopic, dbGetN, dbSearchN, dbGetCategories, dbGetLiked, dbSetLiked, dbGetUserLikes}
